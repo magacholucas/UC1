93d567db53473aadadd1a6d2e9d19e32160720c1
magacholucas <magacho.lucas@gmail.com>
2026-07-06 20:43:54 -0300

    prog25.py alt

---
+++ prog25.py
@@ -0,0 +1,9 @@
+valor1 = int(input ("Digite o primeiro valor "))
+valor2 = int(input ("Digite o primeiro valor "))
+
+if valor1 > valor2: 
+    print ("Valor 1 maior que valor2") 
+elif  valor1 < valor2: 
+    print ("Valor 1 menor que valor2") 
+else :
+    print ("valores iguais")
