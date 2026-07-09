49fb753251aa81a80ae68f40e03d4b3eb0a3dcbf
magacholucas <magacho.lucas@gmail.com>
2026-07-08 21:24:14 -0300

    quarto envio

--- prog33.py
+++ prog33.py
@@ -1,5 +1,7 @@
 x=int(input("Digite o valor "))
 tab=0
 for i in range(0,11):
+    # range(5) é a mesma coisa que o de cima vai de 0 a 5 
     tab=i*x
+    #tab+= x  é a mesma coisa que acima codigo do python
     print (f"{x} x {i} = {tab} ")
---
+++ prog35.py
@@ -0,0 +1,6 @@
+valor = int(input ("informe um valor "))
+y = valor % 2
+if y==0: 
+    print (f"O número {valor} é par") 
+else: 
+    print (f"O número {valor} é impar")
