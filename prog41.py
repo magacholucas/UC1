86cd7377961abb7b82a337a22bc8b88047425c40
magacholucas <magacho.lucas@gmail.com>
2026-07-10 21:03:06 -0300

    Add files via upload

--- prog41.py
+++ prog41.py
@@ -4,5 +4,9 @@ for i in range (2):
     carro= input ("Carro ")
     marca= input ("Marca ")
     valor= float(input("valor: "))
-    carros [carro] = marca, valor
+    carros [carro] = {
+        "marca":marca,
+        "valor":valor
+    }
+    carros [carro] = valor
 print (f"Lista de Carros: {carros}")
