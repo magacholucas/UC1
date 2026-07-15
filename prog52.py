f2871d3c31458363cefdbb641a8d7a43dcb2a02b
magacholucas <magacho.lucas@gmail.com>
2026-07-15 19:34:33 -0300

    teste de commit 15_0701

---
+++ prog52.py
@@ -0,0 +1,8 @@
+def soma(v1, v2):
+    return v1 + v2
+   
+v1 = float(input("valor 1 "))
+v2 = float(input("valor 2 "))
+
+total = soma(v1,v2)
+print (f"o total é {total}")
---
+++ prog54.py
@@ -0,0 +1,7 @@
+def percen(v1):
+    t= v1 *0.15
+    print (f"15% é {t}")
+   
+v1 = float(input("Digite o valor "))
+percen(v1)
+
